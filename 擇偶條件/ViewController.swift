5ca95f27aec50eb7dd5f881956e15c4f084baf08
吕思达 <lyusida@g1pc3n67.g1.ntu.edu.tw>
2017-04-09 15:12:15 +0800

    Initial Commit

---
+++ 擇偶條件/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  擇偶條件
+//
+//  Created by 吕思达 on 2017/4/9.
+//  Copyright © 2017年 吕思达. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
